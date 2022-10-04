233bd9748fccbc1f920d58e7238032cf5132e9aa
Col-R <2colewestin@gmail.com>
2022-10-04 13:52:43 -0500

    worked on some component stuff. struggling with passing props in with typescript

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+
+import storeItems from '../data/data.json'
 
 const Store = () => {
+  interface storeProps {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string
+  }
+  const storeItems:<storeProps> = storeItems.map((item) => {
+    return <StoreItem
+      key = {item.id}
+      item = {item}
+  })
   return (
-    <div>Store</div>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={4} className = "g-3">
+        {storeItems}
+      </Row>
+    </>
   )
 }
 
